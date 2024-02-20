4c94a6e4dd67c539f5ca9ef817cb4d1bbdcf7207
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-02-20 00:34:21 -0500

    Update Asiento.java

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -1,13 +1,15 @@
 package test;
-
 public class Asiento {
 	String color;
 	int precio;
 	int registro;
 	
-	public void cambiarColor (String color) {
-		if (color == "rojo" || color == "amarillo" || color == "verde" || color == "negro" || color == "blanco")
+	void cambiarColor(String color)
+	{
+		if (color == "rojo" || color == "verde" || color == "amarillo" || color == "negro" || color == "blanco")
+		{
 			this.color = color;
-		
+		}
 	}
+
 }
